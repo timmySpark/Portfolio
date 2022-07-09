1f9e9b3cbc822e089206f2bf80b7f42a5e6122e4
timmySpark <timmyspark1@gmail.com>
2022-07-09 15:01:09 +0100

    Created Skills Model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import *
 
 # Register your models here.
+
+admin.site.register(Skills)
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Skills(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+    skill_percent = models.CharField(max_length=5)
+
+    class Meta:
+        verbose_name = ("Skill")
+        verbose_name_plural = ("Skills")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Skills_detail", kwargs={"pk": self.pk})
+
