05b5290ebc66e7f167eacd7c173ee06a4a29d30b
timmySpark <timmyspark1@gmail.com>
2023-10-27 05:12:38 +0100

    [feat]:- renaming files to help find stylings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path , include
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -24,6 +25,9 @@ urlpatterns = [
     path('',include('app.urls'))
 ]
 
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
+
+# if settings.DEBUG:
+#         urlpatterns += static(settings.MEDIA_URL,
+#                               document_root=settings.MEDIA_ROOT)
